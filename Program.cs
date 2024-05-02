95cd7c1bc640f0f8baf2f969ffc7e396368f95a3
Anderson Rodrigues Bernal <andersonbernal2017@gmail.com>
2024-05-02 14:12:27 -0400

    Terminei

--- Program.cs
+++ Program.cs
@@ -1,3 +1,7 @@
 ﻿using DesafioPOO.Models;
 
-// TODO: Realizar os testes com as classes Nokia e Iphone
+Iphone iphone = new Iphone("329749823", "ULTRA", "234HSD0DFA7D0S8", 2333);
+Nokia nokia = new Nokia("254254354", "SUPER", "65SDFSDFASD15DF", 1222);
+
+iphone.InstalarAplicativo("Whatsapp");
+nokia.InstalarAplicativo("Instagram");
